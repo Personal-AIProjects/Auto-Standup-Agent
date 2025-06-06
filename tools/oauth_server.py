13dd498178cbeac8212120cbb27cd066a458638f
Om Patil <patilom1906@gmail.com>
2025-06-06 15:42:38 +0530

    Update default port in OAuth server from 5000 to 5050

--- tools/oauth_server.py
+++ tools/oauth_server.py
@@ -37,6 +37,6 @@ def oauth_redirect():
     return jsonify(resp_json)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host="0.0.0.0", port=port)
 
